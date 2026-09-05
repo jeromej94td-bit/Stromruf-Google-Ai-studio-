9c063993ced3522a4252bf17e4f8308a9522f523
jeromej94td-bit <jeromej94td@gmail.com>
2026-09-05 18:59:14 +0200

    Bump app for Whisper Small quality build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
     applicationId = "com.aistudio.stromruf.gkrfws"
     minSdk = 24
     targetSdk = 36
-    versionCode = 15
-    versionName = "2.4"
+    versionCode = 16
+    versionName = "2.5"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     manifestPlaceholders += mapOf(
