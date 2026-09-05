99e16d1c954aaa8256db0446bde0e94e57b0f1cf
jeromej94td-bit <jeromej94td@gmail.com>
2026-09-05 16:09:02 +0200

    Easybell SIP-Anmeldung wiederherstellen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
     applicationId = "com.aistudio.stromruf.gkrfws"
     minSdk = 24
     targetSdk = 36
-    versionCode = 12
-    versionName = "2.1"
+    versionCode = 13
+    versionName = "2.2"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     manifestPlaceholders += mapOf(
